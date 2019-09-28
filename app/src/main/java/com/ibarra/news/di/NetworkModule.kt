444c625eb3b607e8d5849f518e4fcc842879c0f0
joaoibarra <joao.ibarra@gmail.com>
2019-09-28 13:02:42 -0300

    Add Network Module

---
+++ app/src/main/java/com/ibarra/news/di/NetworkModule.kt
@@ -0,0 +1,47 @@
+package com.ibarra.news.di
+
+import com.ibarra.news.BuildConfig
+import com.ibarra.news.data.remote.IbarraNewsAPi
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+private const val CONNECT_TIMEOUT = 15L
+private const val WRITE_TIMEOUT = 15L
+private const val READ_TIMEOUT = 15L
+
+val NetworkModule = module {
+
+    single<Retrofit> {
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(get())
+            .build()
+    }
+
+    single<IbarraNewsAPi> { get<Retrofit>().create(IbarraNewsAPi::class.java) }
+
+    single{
+        OkHttpClient.Builder().apply {
+            cache(get())
+            connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+            readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            retryOnConnectionFailure(true)
+            addInterceptor(get<Interceptor>())
+            addInterceptor(HttpLoggingInterceptor().apply {
+                if (BuildConfig.DEBUG) {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            })
+        }.build()
+    }
+}
