dbd3f458ab7f61c42e02117581a3a5063b185ae2
joaoibarra <joao.ibarra@gmail.com>
2019-09-28 13:41:41 -0300

    Add source folder with Activity and ViewModel

---
+++ app/src/main/java/com/ibarra/news/ui/source/SourceNewsActivity.kt
@@ -0,0 +1,13 @@
+package com.ibarra.news.ui.source
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ibarra.news.R
+
+class SourceNewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_source_news)
+    }
+}
---
+++ app/src/main/java/com/ibarra/news/ui/source/SourceNewsViewModel.kt
@@ -0,0 +1,4 @@
+package com.ibarra.news.ui.source
+
+class SourceNewsViewModel {
+}
